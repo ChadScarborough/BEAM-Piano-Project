58734262a97df4f7014baf63f5df1883525f493b
Ripsky-Korsakov <chad.d.scarborough@gmail.com>
2023-03-06 12:56:49 -0600

    Initial Commit

---
+++ GamePadTestScript.py
@@ -0,0 +1,15 @@
+import USB_Device as dev
+import XBox360_Driver as xbd3
+import XBoxOne_Driver as xbd1
+import Gamecube_Driver as gcd
+import Shitty_Driver as sd
+
+#xb360 = dev.Device(0x20d6, 0x200e, 0, 1)
+#xb1 = dev.Device(0x24c6, 0x541a, 0, 1)
+#gc1 = dev.Device(0x20d6, 0xa711, 0, 1)
+#gc2 = dev.Device(0x20d6, 0xa711, 0, 1, 1)
+#sc = dev.Device(0x0428, 0x4001, 0, 0)
+
+while True:
+	#r = sd.parse_input(sc.get_device_readout())
+	#print(r)
